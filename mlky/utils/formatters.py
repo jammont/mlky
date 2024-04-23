f92ef0c19096252f71df67674bd53aedc124ed33
James Montgomery <J.Montgomery@jpl.nasa.gov>
2024-04-23 16:08:30 -0700

    Fixed formatter from mismatching left and right values

--- mlky/utils/formatters.py
+++ mlky/utils/formatters.py
@@ -47,11 +47,14 @@ def printTable(iterable, enum=False, delimiter='=', offset=1, prepend='', trunca
     right = []
     for item in iterable:
         left.append(str(item[0]))
+
         if len(item) > 1:
             right.append(str(item[1]))
+        else:
+            right.append(None)
 
-    if not right:
-        return left
+    if not left:
+        return []
     elif isinstance(truncate, int):
         left = [item[:truncate] for item in left]
 
@@ -61,16 +64,15 @@ def printTable(iterable, enum=False, delimiter='=', offset=1, prepend='', trunca
         formatter += '{i:' + f'{len(str(len(iterable)))}' + '}: '
     formatter += '{left:'+ str(padding) + '}' + delimiter + ' {right}'
 
-    i = 0
     formatted = []
-    for item in iterable:
+    for i, item in enumerate(iterable):
         if len(item) > 1:
             string = formatter.format(i=i, left=left[i], right=right[i])
             if len(item) > 2:
                 formatted.append((string, *item[2:]))
             else:
                 formatted.append((string, ))
-            i += 1
+
         else:
             formatted.append(item)
 
