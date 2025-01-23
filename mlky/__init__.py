214857050b4a1ceb94c31e3a450f46eadf1a40fa
James Montgomery <J.Montgomery@jpl.nasa.gov>
2025-01-23 12:06:52 -0800

    Fixed package name

--- mlky/__init__.py
+++ mlky/__init__.py
@@ -2,7 +2,7 @@
 """
 import importlib.metadata
 
-__version__ = importlib.metadata.version("mypackage")
+__version__ = importlib.metadata.version("mlky")
 
 # Instantiate before the CLI
 from mlky.configs import *
